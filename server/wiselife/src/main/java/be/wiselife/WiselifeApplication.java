83f8d0c638790dbe9537f56522c6423afc9b096d
msKim92 <mins402kim@gmail.com>
2022-11-11 11:45:25 +0900

    Setting wiselife Main branch

---
+++ server/wiselife/src/main/java/be/wiselife/WiselifeApplication.java
@@ -0,0 +1,13 @@
+package be.wiselife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiselifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiselifeApplication.class, args);
+	}
+
+}
---
+++ server/wiselife/src/test/java/be/wiselife/WiselifeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.wiselife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiselifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
