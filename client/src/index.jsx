78747053bd6a487a3edb7e7c453004dfb10d0bdf
OneMoreBottlee <hanbj0301@gmail.com>
2022-11-14 13:42:49 +0900

    feat: 환경설정

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './common.css';
+import './style/common.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
