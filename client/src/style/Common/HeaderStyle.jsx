b8622f3f72b88bafea1aa2b5a6338b869103856b
leeseoshim <leeseoit@gmail.com>
2022-11-14 23:57:07 +0900

    feat: header작업, react icon다운로드

---
+++ client/src/style/Common/HeaderStyle.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+import { FaSearch } from 'react-icons/fa';
+export const HeaderContainer = styled.header`
+  border-bottom: 1px solid #e6e6e6;
+  width: 99.8%;
+  padding: 0.7%;
+`;
+
+export const Container = styled.div`
+  width: 1024px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled.div`
+  color: #8673ff;
+`;
+
+export const ChallengeBtn = styled.button`
+  border: 0;
+  outline: 0;
+  background-color: #ffffff;
+  color: #787878;
+`;
+
+export const Search = styled.div`
+  position: relative;
+  width: 500px;
+  & input {
+    width: 100%;
+    padding: 10px 12px;
+    border-radius: 30px;
+    border: 1px solid #e6e6e6;
+    font-size: 14px;
+    box-shadow: 3px 7px 12px 0 #c2c2c2;
+  }
+`;
+
+export const Icon = styled(FaSearch)`
+  width: 17px;
+  position: absolute;
+  top: 10px;
+  right: 0;
+  margin: 0;
+  color: #787878;
+`;
