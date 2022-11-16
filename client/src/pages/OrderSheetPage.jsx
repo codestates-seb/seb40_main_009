9292ddb3882db0843f9bd2e94659829af256c808
Aurora <eunbipower@gmail.com>
2022-11-16 16:56:26 +0900

    style: 결제페이지, 마이페이지 프로필 css 수정

--- client/src/pages/OrderSheetPage.jsx
+++ client/src/pages/OrderSheetPage.jsx
@@ -1,67 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
 import OrderSheetInfo from '../components/OrderSheetList/OrderSheetInfo';
-
-const OrderSheetInfoPageComponent = styled.div`
-  width: 1024px;
-  font-size: 18px;
-  color: #595959;
-  margin: auto;
-  .order-sheet {
-    border-top: 0.2rem solid #595959;
-    border-bottom: 0.12rem solid #595959;
-  }
-  .order-lists {
-    display: flex;
-  }
-  .challenge-title {
-    margin: 1rem 0;
-  }
-  .challenge-date {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-  .image-size {
-    width: 200px;
-    height: 200px;
-    margin: 2.5rem 2.5rem 2.5rem 0;
-  }
-  .order-title {
-    font-size: 24px;
-    margin: 1rem 0;
-  }
-  .order-info {
-    display: flex;
-    margin-top: 1rem;
-  }
-  .order-left {
-    width: 100%;
-  }
-  .order-info-top {
-    line-height: 3rem;
-    padding-bottom: 4rem;
-  }
-  .order-info-bottom {
-    line-height: 3rem;
-    border-top: 0.12rem solid #595959;
-    padding-top: 1rem;
-    padding-bottom: 4rem;
-  }
-  .pay-button {
-    width: 200px;
-    height: 60px;
-    background-color: #fbe34d;
-    border-radius: 10px;
-    border: none;
-    cursor: pointer;
-  }
-
-  .order-right {
-    margin: 1rem 0rem 0rem 1rem;
-  }
-`;
+import * as S from '../style/OrderSheetListStyle/OrderSheetPageStyle.jsx';
 
 function OrderSheetPage({ money }) {
   const [orderList, setOrderList] = useState({
@@ -79,7 +19,7 @@ function OrderSheetPage({ money }) {
     });
   }, []);
   return (
-    <OrderSheetInfoPageComponent>
+    <S.OrderSheetInfoPageComponent>
       <h1 className="order-title">주문서 작성</h1>
       <header className="order-sheet">
         <div className="order-lists">
@@ -106,7 +46,7 @@ function OrderSheetPage({ money }) {
           <OrderSheetInfo money={orderList.money} />
         </div>
       </main>
-    </OrderSheetInfoPageComponent>
+    </S.OrderSheetInfoPageComponent>
   );
 }
 
