c23c488fcd21e5d2ba2de9d5d92c17caab86dedd
Aurora <eunbipower@gmail.com>
2022-11-16 09:33:11 +0900

    feat: 결제페이지 구축

--- client/src/components/OrderSheetList/OrderSheetInfo.jsx
+++ client/src/components/OrderSheetList/OrderSheetInfo.jsx
@@ -1,20 +1,57 @@
 import styled from 'styled-components';
 
 const OrderSheetInfoComponent = styled.div`
-  border: 0.1rem solid #595959;
-  width: 100%;
-  height: 450px;
+  .info-container {
+    width: 350px;
+    height: 400px;
+    padding: 1rem;
+    border: 0.12rem solid #595959;
+    border-radius: 10px;
+  }
+  .info-container-lists {
+    padding: 1rem;
+    line-height: 2rem;
+  }
+  .order-info-title {
+    font-size: 24px;
+    margin: 1rem 0;
+  }
+  .check-and-pay {
+    padding-top: 2rem;
+    margin-left: 2rem;
+  }
+  .check-input-box {
+    margin-left: 25%;
+  }
+  .order-button {
+    color: #ffffff;
+    font-size: 24px;
+    width: 250px;
+    height: 80px;
+    margin-top: 1rem;
+    background-color: #8673ff;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+  }
 `;
 function OrderSheetInfo({ money }) {
   return (
     <OrderSheetInfoComponent>
-      <h1>결제 정보</h1>
-      <h2>참가비 : {money}원</h2>
-      <div>100% 성공 시 : 참가비 + 상금</div>
-      <div>80% 성공 시 : 달성률에 따른 원금 + 상금</div>
-      <div>50% 성공 시 : 달성률에 따른 원금 + 상금</div>
-      <input type="checkbox"></input>
-      <button>결제하기</button>
+      <form className="info-container">
+        <div className="info-container-lists">
+          <h1 className="order-info-title">결제 정보</h1>
+          <h2>참가비 : {money}원</h2>
+          <div>100% 성공 시 : 참가비 + 상금</div>
+          <div>80% 성공 시 : 달성률에 따른 원금 + 상금</div>
+          <div>50% 성공 시 : 달성률에 따른 원금 + 상금</div>
+          <div className="check-and-pay">
+            <input className="check-input-box" type="checkbox" />
+            <span>동의합니다</span>
+            <button className="order-button">결제하기</button>
+          </div>
+        </div>
+      </form>
     </OrderSheetInfoComponent>
   );
 }
--- client/src/pages/OrderSheetPage.jsx
+++ client/src/pages/OrderSheetPage.jsx
@@ -5,49 +5,48 @@ import OrderSheetInfo from '../components/OrderSheetList/OrderSheetInfo';
 
 const OrderSheetInfoPageComponent = styled.div`
   width: 1024px;
+  font-size: 18px;
   color: #595959;
-  font-size: 20px;
-
-  h1 {
-    font-size: 24px;
-    font-weight: bold;
-    border-bottom: 0.2rem solid #595959;
-    padding-bottom: 1rem;
+  .order-sheet {
+    border-top: 0.2rem solid #595959;
+    border-bottom: 0.12rem solid #595959;
   }
-  .container {
+  .order-lists {
     display: flex;
-    /* flex-direction: column; */
   }
-`;
-const OrderSheet = styled.div`
-  .image-size {
-    width: 200px;
-    height: 200px;
-    margin-right: 2.5rem;
+  .challenge-title {
+    margin: 1rem 0;
   }
-  h2 {
-    font-size: 24px;
-    font-weight: bold;
-    border-top: 0.1rem solid #595959;
-    padding: 2rem 0;
-  }
-  .make-order-sheets {
-    display: flex;
-    margin: 2.5rem 0;
-  }
-
-  .make-order-sheet {
+  .challenge-date {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    .challenge-tittle {
-      font-size: 20px;
-      line-height: 2.5rem;
-    }
   }
-  .price-order-sheet {
-    line-height: 2.5rem;
-    margin: 2.5rem 0;
+  .image-size {
+    width: 200px;
+    height: 200px;
+    margin: 2.5rem 2.5rem 2.5rem 0;
+  }
+  .order-title {
+    font-size: 24px;
+    margin: 1rem 0;
+  }
+  .order-info {
+    display: flex;
+    margin-top: 1rem;
+  }
+  .order-left {
+    width: 100%;
+  }
+  .order-info-top {
+    line-height: 3rem;
+    padding-bottom: 4rem;
+  }
+  .order-info-bottom {
+    line-height: 3rem;
+    border-top: 0.12rem solid #595959;
+    padding-top: 1rem;
+    padding-bottom: 4rem;
   }
   .pay-button {
     width: 200px;
@@ -57,8 +56,9 @@ const OrderSheet = styled.div`
     border: none;
     cursor: pointer;
   }
-  .left-order {
-    width: 60%;
+
+  .order-right {
+    margin: 1rem 0rem 0rem 1rem;
   }
 `;
 
@@ -79,38 +79,32 @@ function OrderSheetPage({ money }) {
   }, []);
   return (
     <OrderSheetInfoPageComponent>
-      <h1>주문서 작성</h1>
-      <OrderSheet>
-        <div className="make-order-sheets">
+      <h1 className="order-title">주문서 작성</h1>
+      <header className="order-sheet">
+        <div className="order-lists">
           <img className="image-size" src={orderList.image} alt="product img" />
-          <div className="make-order-sheet">
-            <div className="challenge-tittle">{orderList.name}</div>
+          <div className="challenge-date">
+            <p className="challenge-title">{orderList.name}</p>
             <p>시작 날짜 : {orderList.startPeriod}</p>
             <p>종료 날짜 : {orderList.expirationPeriod}</p>
           </div>
         </div>
-        <div style={{ display: 'flex' }}>
-          <div
-            style={{
-              border: '1px solid red',
-              marginRight: 'auto',
-              width: '100%',
-            }}
-          >
-            <div className="price-order-sheet">
-              <h2>주문정보</h2>
-              <p>참가비 : {orderList.money}원</p>
-            </div>
-            <div>
-              <h2>결제 수단</h2>
-              <button className="pay-button">카카오페이</button>
-            </div>
+      </header>
+      <main className="order-info">
+        <div className="order-left">
+          <div className="order-info-top">
+            <h2 className="order-title">주문 정보</h2>
+            <p>참가비 : {orderList.money}원</p>
           </div>
-          <div style={{ border: '1px solid blue' }}>
-            <OrderSheetInfo money={orderList.money} />
+          <div className="order-info-bottom">
+            <h2 className="order-title">결제 수단</h2>
+            <button className="pay-button">카카오페이</button>
           </div>
         </div>
-      </OrderSheet>
+        <div className="order-right">
+          <OrderSheetInfo money={orderList.money} />
+        </div>
+      </main>
     </OrderSheetInfoPageComponent>
   );
 }
