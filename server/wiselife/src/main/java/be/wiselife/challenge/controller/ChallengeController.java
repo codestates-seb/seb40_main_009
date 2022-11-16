a8ab2adeda15ad1a9b600d4b7417d953050f1ea6
you88311@naver.com <you88311@naver.com>
2022-11-16 15:12:35 +0900

    chore: challenge 상세페이지 조회 관련 주석 수정

--- server/wiselife/src/main/java/be/wiselife/challenge/controller/ChallengeController.java
+++ server/wiselife/src/main/java/be/wiselife/challenge/controller/ChallengeController.java
@@ -49,14 +49,14 @@ public class ChallengeController {
     }
 
     /*챌린지 상세페이지 조회*/
+    /* userId는 optional parameter
+     *
+     * 추후 userId 이용한 MemberChallenge 엔티티 구현 후 추가 해야 하는 기능
+     * 1) 만약 유저가 해당 챌린지 참여중이라면 별도로 유저의 해당 챌린지 성공률도 표시함
+     * 2) 챌린지 참여중인 유저들의 평균 챌린지 성공률
+     * */
     @GetMapping("{challenge-id}")
     public ResponseEntity getChallenge(@PathVariable("challenge-id") @Positive Long challengeId,
-                                       /* userId는 optional parameter
-                                       *
-                                       * 추후 MemberChallenge 엔티티 구현 후 추가 해야 하는 기능
-                                       * 1) 만약 유저가 해당 챌린지 참여중이라면 별도로 유저의 해당 챌린지 성공률도 표시함
-                                       * 2) 챌린지 참여중인 유저들의 평균 챌린지 성공률
-                                       * */
                                        @RequestParam(value = "userId", required = false) Long userIdOrNull){
 
         Challenge challenge = challengeService.getChallenge(challengeId); //챌린지 찾기
