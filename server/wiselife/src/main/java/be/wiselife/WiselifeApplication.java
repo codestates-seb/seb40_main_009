16f9445014ab74b9fc4ab64301641ea3624bff80
dbgys1127 <dbgys1127@gmail.com>
2022-11-28 18:39:22 +0900

    feat: jpa mysql 연동 설정 추가

--- server/wiselife/src/main/java/be/wiselife/WiselifeApplication.java
+++ server/wiselife/src/main/java/be/wiselife/WiselifeApplication.java
@@ -2,6 +2,7 @@ package be.wiselife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
