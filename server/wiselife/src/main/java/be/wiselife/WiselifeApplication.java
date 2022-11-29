5529d0324a6f78575b76fc7f4b778c8716075e06
you88311@naver.com <you88311@naver.com>
2022-11-29 23:26:10 +0900

    feat: aws 연결 관련 yml파일 추가

--- server/wiselife/src/main/java/be/wiselife/WiselifeApplication.java
+++ server/wiselife/src/main/java/be/wiselife/WiselifeApplication.java
@@ -12,6 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class WiselifeApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("aws.accessKeyId", "{AWS_ACCESS_KEY_ID}");
+		System.setProperty("aws.secretKey", "{AWS_SECRET_ACCESS_KEY}");
+		System.setProperty("aws.region", "ap-northeast-2");
 		SpringApplication.run(WiselifeApplication.class, args);
 	}
 
