e36cc7c15fef4ce66d4948f1bf1ccd0fddfc04cc
msKim92 <mins402kim@gmail.com>
2022-11-29 13:10:51 +0900

    fix: gitActions settings

--- server/wiselife/Scripts/remove_before.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find /opt/codedeploy-agent/deployment-root/배포디렉토리/* -maxdepth 0 -type 'd' | grep -v
-$(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/배포디렉토리/* | sort -t: -n | tail
--1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
-
-sudo rm -rf /home/ec2-user/* # 이전 배포내용을 지움
