0c57ee242040fc2250b6c00ef02b85620dc5dd52
Aurora-in-Wonderland <99107568+Aurora-in-Wonderland@users.noreply.github.com>
2022-12-06 18:32:22 +0900

    Update ChllengeList.styled.jsx

--- client/src/style/ChallengeList/ChllengeList.styled.jsx
+++ client/src/style/ChallengeList/ChllengeList.styled.jsx
@@ -1,49 +1,80 @@
 import styled from 'styled-components';
 
-export const ChallengeListContainer = styled.section`
+export const ListContainer = styled.section`
+  padding-top: 60px;
+  width: 1300px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-content: center;
 
-  > button {
-    width: 100px;
-    height: 40px;
-    border-radius: 15px 15px 0 0;
-    text-align: center;
-    top: 60px;
-    position: absolute;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.2);
-    cursor: pointer;
-
-    &:nth-child(1) {
+  > :nth-child(1) {
+    margin-top: 20px;
+    > a {
+      > button {
+        :hover {
+          background-color: #8673ff;
+          color: white;
+        }
+      }
+      :hover {
+        color: white;
+      }
     }
-
-    &:nth-child(2) {
-      left: 110px;
-    }
-
-    &:nth-child(3) {
-      left: calc(100% - 210px);
-    }
-    &:nth-child(4) {
-      left: calc(100% - 100px);
-    }
-
-    :hover {
-      background-color: #8673ff;
-      color: white;
+    > div {
+      position: relative;
+      > select {
+        position: absolute;
+        width: 130px;
+        height: 30px;
+        top: -30px;
+        right: 0px;
+      }
     }
   }
 `;
 
 export const Container = styled.section`
-  margin-top: 100px;
   padding: 92px 30px 80px 30px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 20px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const AddChallengeButton = styled.button`
+  position: fixed;
+  width: 70px;
+  height: 70px;
+  border-radius: 100px;
+  z-index: 3;
+  top: calc(100% - 150px);
+  right: 20%;
+  background-color: white;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+
+  :hover {
+    background-color: #d3dbfc;
+    color: #8673ff;
+  }
+
+  :active {
+    background-color: #8673ff;
+    color: white;
+  }
+`;
+
+export const Tab = styled.button`
+  width: 100px;
+  height: 40px;
+  position: relative;
+  border-radius: 15px 15px 0 0;
+  text-align: center;
   border: 1px solid rgba(0, 0, 0, 0.2);
-  box-shadow: 5px 5px 14px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  background-color: ${(props) => (props.isActive ? `#8673ff` : `white`)};
+  color: ${(props) => (props.isActive ? `white` : 'black')};
 `;
