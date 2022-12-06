a337d3aff03185c4ddad1059fabf266e3ab1c1f7
leeseoshim <leeseoit@gmail.com>
2022-12-07 01:59:04 +0900

    fix: 마이페이지 도전내역 탭 수정

--- client/src/components/ProfileList/ProfileBoxLists/ProfileBoxChallenge.jsx
+++ client/src/components/ProfileList/ProfileBoxLists/ProfileBoxChallenge.jsx
@@ -11,53 +11,77 @@ function ProfileBoxList({
   memberSuccessDay,
   objDay,
   memberChallengeSuccessRate,
+  endChallengesData,
 }) {
   const navigate = useNavigate();
+  // console.log('endChallengesData>>', endChallengesData);
 
   // 챌린지 클릭시 이동
   const onClickChallenge = () => {
     navigate(`/detail/${challengeId}`);
   };
 
+  // console.log('clickedTab>>', clickedTab);
+  // console.log('challengeId>>', challengeId);
   // 문제 => 각각의 탭 잘 나오다 0번째 탭을 클릭하고 이동하면 모두 0번 째 내용으로만 나옴
   return (
-    <ProfileBoxChallengeComponent onClick={onClickChallenge}>
-      <div className="title">
-        <h1>
-          {clickedTab === 0 && '도전 중'}
-          {clickedTab === 1 && '도전내역'}
-          {clickedTab === 2 && '결제내역'}
-          {clickedTab === 0 && <span className="tag">NOW</span>}
-          {/* 도전중 내전 내역 통합 */}
-        </h1>
-        <span className="percentage">{memberChallengeSuccessRate}%</span>
-      </div>
-      <form className="challenge-box">
-        <img
-          src={challengeRepImage}
-          className="challenge-box-image"
-          alt="challenge-box-img"
-        />
-        <div className="challenge-box-info">
-          <p>{challengeTitle}</p>
-          <div className="challenge-box-info-lists">
-            <p>
-              성공일수:{memberSuccessDay} / 목표일수:{objDay}
-            </p>
-            <p>
-              예상환급금:&nbsp;
-              {memberReward
-                .toString()
-                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
-              &nbsp; 원
-            </p>
-          </div>
+    <>
+      {clickedTab === 1 && endChallengesData === [] ? (
+        <div
+          role="img"
+          aria-label="writing hand"
+          style={{
+            width: '100%',
+            height: '450px',
+            marginTop: '1%',
+            fontSize: '20px',
+            display: 'flex',
+            justifyContent: 'center',
+            borderRadius: '20px',
+            alignItems: 'center',
+          }}
+        >
+          인증사진을 올려주세요.😊
         </div>
-      </form>
-      <p className="notice">
-        *수수료로 인해 예상금액과 실제 환급금액은 서로 상이할 수 있음.
-      </p>
-    </ProfileBoxChallengeComponent>
+      ) : (
+        <ProfileBoxChallengeComponent onClick={onClickChallenge}>
+          <div className="title">
+            <h1>
+              {clickedTab === 0 && '도전 중'}
+              {clickedTab === 1 && '도전내역'}
+              {clickedTab === 2 && '결제내역'}
+              {clickedTab === 0 && <span className="tag">NOW</span>}
+            </h1>
+            <span className="percentage">{memberChallengeSuccessRate}%</span>
+          </div>
+          <form className="challenge-box">
+            <img
+              src={challengeRepImage}
+              className="challenge-box-image"
+              alt="challenge-box-img"
+            />
+            <div className="challenge-box-info">
+              <p>{challengeTitle}</p>
+              <div className="challenge-box-info-lists">
+                <p>
+                  성공일수:{memberSuccessDay} / 목표일수:{objDay}
+                </p>
+                <p>
+                  예상환급금:&nbsp;
+                  {memberReward
+                    .toString()
+                    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+                  &nbsp; 원
+                </p>
+              </div>
+            </div>
+          </form>
+          <p className="notice">
+            *수수료로 인해 예상금액과 실제 환급금액은 서로 상이할 수 있음.
+          </p>
+        </ProfileBoxChallengeComponent>
+      )}
+    </>
   );
 }
 
--- client/src/components/ProfileList/ProfileBoxLists/ProfileBoxLists.jsx
+++ client/src/components/ProfileList/ProfileBoxLists/ProfileBoxLists.jsx
@@ -15,6 +15,7 @@ function ProfileBoxLists({
 }) {
   const [clickedTab, setClickedTab] = useState(0); //router 사용하기
   const [challenges, setChallenges] = useState(participatingChallenges);
+  const [endChallengesData, setEndChallengesData] = useState(endChallenges);
 
   const LoginName = localStorage.getItem('LoginName');
   const tabName = {
@@ -27,13 +28,16 @@ function ProfileBoxLists({
     if (clickedTab === 0) {
       setChallenges(participatingChallenges);
     }
+    // if (clickedTab === 1) {
+    //   setChallenges(endChallenges);
+    // }
     if (clickedTab === 1) {
-      setChallenges(endChallenges);
+      setEndChallengesData(endChallengesData);
     }
     if (clickedTab === 2) {
       setChallenges([]); // 빈배열을 주는 이유: 아래 내용을 빈배열로 만들고 새로운 내용 넣음
     }
-  }, [participatingChallenges, endChallenges, clickedTab]);
+  }, [participatingChallenges, endChallengesData, clickedTab]);
 
   return (
     <ProfileBoxComponent>
@@ -66,6 +70,7 @@ function ProfileBoxLists({
               objDay={challenge.objDay}
               memberChallengeSuccessRate={challenge.memberChallengeSuccessRate}
               clickedTab={clickedTab}
+              endChallengesData={endChallengesData}
             />
           ))}
         {clickedTab === 2 && memberName === LoginName && (
