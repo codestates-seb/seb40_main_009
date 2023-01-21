e64852f8a6066584193facf4e65fa03ee46495d9
msKim92 <mins402kim@gmail.com>
2023-01-21 23:00:22 +0900

    fix: discord rules

--- server/wiselife/src/main/java/be/wiselife/aop/ServerDeployChecker.java
+++ server/wiselife/src/main/java/be/wiselife/aop/ServerDeployChecker.java
@@ -1,5 +1,6 @@
 package be.wiselife.aop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -12,11 +13,12 @@ import java.net.UnknownHostException;
 @Configuration
 public class ServerDeployChecker{
 
+    @Value("${discord.depoly}")
+    private String depolyUrl;
 
     @EventListener(ApplicationReadyEvent.class)
     public void initData() {
-        DiscordWebhook webhook = new DiscordWebhook
-                ("https://discord.com/api/webhooks/1045570083558273044/AErMVlgJw6eAMfjz3Q1sgiRNtp35WGtjpLKOWkQN7fZkesUxGAyPnVWB-zxMkoeYyKv1");
+        DiscordWebhook webhook = new DiscordWebhook(depolyUrl);
 
         try {
             String ip = InetAddress.getLocalHost().getHostAddress();
